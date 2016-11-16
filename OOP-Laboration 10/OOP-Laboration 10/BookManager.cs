fb5ab5fe20fa7acc2328920d6f4e9c5ff470a52c
Anders Tingstam <anders.tingstam@gmail.com>
2016-11-16 13:08:36 +0100

    Started labb 10

---
+++ OOP-Laboration 10/OOP-Laboration 10/BookManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Laboration_10
+{
+    class BookManager
+    {
+        public List<Book> Books { get; set; }
+        public delegate bool BookFilter(Book book);
+
+
+        public void PrintWhere(BookFilter filter)
+        {
+
+        }
+    }
+}
